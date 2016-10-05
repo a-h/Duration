d9137afe1dd2281ed42f0dadfaae6e47e3e82efd
Adrian Hesketh <adrianhesketh@hushmail.com>
2016-10-05 15:31:30 +0100

    First checkin of duration timer.

---
+++ DurationTimer.Tests/Test.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using System;
+using System.Threading;
+using System.Linq;
+
+namespace DurationTimer.Tests
+{
+	[TestFixture()]
+	public class Test
+	{
+		[Test]
+		public void TestTimer()
+		{
+			var capturedMessage = "";
+			Action<string> logger = msg => capturedMessage = msg;
+
+			using (var timer = new TimeLogger(logger, "logentry"))
+			{
+				Thread.Sleep(5);
+			}
+
+			Console.WriteLine(capturedMessage);
+
+			Assert.IsTrue(HasExpectedLogEntry(capturedMessage, "logentry", 5, 10));
+		}
+
+		[Test]
+		public void TestExpectations()
+		{
+			Assert.True(HasExpectedLogEntry("a=1ms", "a", 1, 1), "1ms is in the range of 1-1");
+			Assert.False(HasExpectedLogEntry("a=0ms", "a", 1, 1), "0ms is not in the range of 1-1");
+			Assert.True(HasExpectedLogEntry("a=10ms", "a", 5, 11), "10ms is in the range of 5-11");
+			Assert.False(HasExpectedLogEntry("a=12ms", "a", 5, 11), "12ms is not in the range of 5-11");
+			Assert.False(HasExpectedLogEntry("a=12ms", "b", 5, 12), "Expected to that 'b' would not match 'a' was not expected.");
+		}
+
+		private bool HasExpectedLogEntry(string entry, string expectedName, int min, int max)
+		{
+			var actualName = entry.Split('=').First();
+
+			if (actualName != expectedName)
+			{
+				return false;
+			}
+
+			var ms = int.Parse(entry.Split('=').Last().TrimEnd("ms".ToCharArray()));
+
+			return ms >= min && ms <= max;
+		}
+	}
+}
---
+++ DurationTimer/DurationTimer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace DurationTimer
+{
+	public class TimeLogger : IDisposable
+	{
+		Stopwatch stopWatch = new Stopwatch();
+
+		public Action<string> Logger { get; set; }
+		public string MetricName { get; set; }
+
+		public TimeLogger(Action<string> logger, string metricName)
+		{
+			this.Logger = logger;
+			this.MetricName = metricName;
+
+			stopWatch.Start();
+		}
+
+		public void Dispose()
+		{
+			stopWatch.Stop();
+			this.Logger($"{this.MetricName}={stopWatch.ElapsedMilliseconds}ms");
+		}
+	}
+}
+
